0714a381cca6041999b93888783b53b7bfac86f4
Jose Peinado <60111158+BreckDesu@users.noreply.github.com>
2020-01-22 12:59:02 -0700

    Hola Mundo

---
+++ Clase_1/Practica 1/Program.cs
@@ -0,0 +1,16 @@
+using System;
+// importamos la liberia principal.
+
+namespace Practica_1
+{
+    class Program
+    {
+        static void Main(string args)
+        {
+            Console.WriteLine("Hola bb mundo");                                                                                                                                                                                      
+        
+        }
+
+
+    }
+}
