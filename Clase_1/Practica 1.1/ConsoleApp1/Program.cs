dbe5af097fcf9b7cfcaf4ac8d45eb2674f1cc632
Jose Peinado <60111158+BreckDesu@users.noreply.github.com>
2020-01-27 12:23:50 -0700

    Actualizacion practica 1

---
+++ Clase_1/Practica 1.1/ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static string nombre = "Roberto";
+        static void Main(string[] args)
+        {
+
+            if (args[0] == nombre)
+            {
+                Console.WriteLine($"Hello {args[0]}");
+            }
+            else
+            {
+                Console.WriteLine("User Unauthorized");
+            }
+        }
+
+    }
+}
