936c4175694e9df6698bf5edc29f958eb81e635b
Jose Peinado <60111158+BreckDesu@users.noreply.github.com>
2020-02-07 12:53:47 -0700

    practica 2

---
+++ Clase_1/Practica 2/Practica 2/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace Practica_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("CALCULADORA MAMADISIMA");
+            int res;
+            int val1;
+            int val2;
+            string op;
+
+            Console.WriteLine("Ingresa numero 1");
+            val1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Ingresa numero 2");
+            val2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Que operacion vas a realizar?");
+            op = Console.ReadLine();
+
+
+            switch (op)
+            {
+                case "+":
+                    res = val1 + val2;
+                    Console.WriteLine("Tu resultado es "+res);
+
+                    break;
+                case "-":
+                    res = val1 - val2;
+                    Console.WriteLine("Tu resultado es " + res);
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                             
+                    break;
+                case "/":
+                    res = val1 / val2;
+                    Console.WriteLine("Tu resultado es " + res);
+
+                    break;
+                case "*":
+                    res = val1 * val2;
+                    Console.WriteLine("Tu resultado es " + res);
+
+                    break;
+                default:
+                    Console.WriteLine("OPERADOR NO VALIDO");
+
+                    break;
+            }
+        }
+    }
+}
