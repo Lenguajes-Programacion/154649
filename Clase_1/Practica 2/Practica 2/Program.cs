80ee98d6c177b7f202436ac3df381f0c821889f9
Daniel Olivas <60111295+DanielOIOD@users.noreply.github.com>
2020-02-10 12:40:22 -0700

    While

--- Clase_1/Practica 2/Practica 2/Program.cs
+++ Clase_1/Practica 2/Practica 2/Program.cs
@@ -11,54 +11,88 @@ namespace Practica_2
             double val2;
             string op;
             string op2;
-            Console.WriteLine("CALCULADORA MAMADISIMA");
+            bool cerrar = true;
 
-            Console.WriteLine("Vas a realizar otra operacion (s/n)?");
-            op2 = Console.ReadLine();
+           
+          
 
-            while (op2 == "s")
+            while (cerrar)
             {
+             
                 Console.Clear();
+                Console.WriteLine("CALCULADORA MAMADISIMA");
+                Console.WriteLine("----------------------------------------------");
                 Console.WriteLine("Ingresa numero 1");
                 val1 = Convert.ToInt32(Console.ReadLine());
                 Console.WriteLine("Ingresa numero 2");
                 val2 = Convert.ToInt32(Console.ReadLine());
-                Console.WriteLine("Que operacion vas a realizar?");
+                Console.WriteLine("----------------------------------------------");
+                Console.WriteLine(" + para suma");
+                Console.WriteLine(" - para resta");
+                Console.WriteLine(" * para multiplicacion");
+                Console.WriteLine(" / para divicion");
+                Console.WriteLine(" s para salir");
+                Console.WriteLine("----------------------------------------------");
                 op = Console.ReadLine();
-
-
+                
                 switch (op)
                 {
                     case "+":
                         res = val1 + val2;
+                        Console.WriteLine("----------------------------------------------");
                         Console.WriteLine("Tu resultado es " + res);
+                    
 
                         break;
                     case "-":
                         res = val1 - val2;
+                        Console.WriteLine("----------------------------------------------");
                         Console.WriteLine("Tu resultado es " + res);
+                       
 
                         break;
                     case "/":
                         res = val1 / val2;
+                        Console.WriteLine("----------------------------------------------");
                         Console.WriteLine("Tu resultado es " + res);
+                     
 
                         break;
                     case "*":
                         res = val1 * val2;
+                        Console.WriteLine("----------------------------------------------");
                         Console.WriteLine("Tu resultado es " + res);
+                       
+                        break;
+                    case "s":
+
+                        cerrar = false;
 
                         break;
+                    
                     default:
+                        Console.WriteLine("----------------------------------------------");
                         Console.WriteLine("OPERADOR NO VALIDO");
 
                         break;
 
                 }
-                Console.WriteLine(" ");
-                Console.WriteLine(" ");
-                Console.WriteLine("Vas a realizar otra operacion (s/n)?");
+                Console.WriteLine("");
+                Console.WriteLine("");
+                Console.WriteLine("");
+                Console.WriteLine("Deseas realizar otra operacion (s/n)");
                 op2 = Console.ReadLine();
+
+                if (op2 == "s")
+                {
+                    cerrar = true;
+                }
+                else
+                {
+                    cerrar = false;
+                }
+
+
 
             }
 
