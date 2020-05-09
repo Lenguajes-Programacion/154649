200ab09922d4c0138265c478b2867db5ee1c9a06
Jose Peinado <60111158+BreckDesu@users.noreply.github.com>
2020-05-08 21:56:02 -0600

    Add files via upload

---
+++ Calculadora/Calculadora/Operaciones/Operac.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace Calculadora.Operaciones
+{
+    class Operac
+    {
+        public double a;
+        public double b;
+
+        public static double Suma(double a, double b)
+        {
+            double Res = 0;
+            Res = a + b;
+            return Res;
+        }
+        public static double Resta(double a, double b)
+        {
+            double Res = 0;
+            Res = a - b;
+            return Res;
+        }
+        public static double Divicion(double a, double b)
+        {
+            double Res = 0;
+            Res = a / b;
+            return Res;
+        }
+        public static double Multiplicacion(double a, double b)
+        {
+            double Res = 0;
+            Res = a * b;
+            return Res;
+        }
+        public void Potencia(double a, double b)
+        {
+            double Pot = Math.Pow(a, b);
+            Console.WriteLine(" {0} elevado a {1} es {2}", a, b, Pot);
+        }
+
+    }
+}
